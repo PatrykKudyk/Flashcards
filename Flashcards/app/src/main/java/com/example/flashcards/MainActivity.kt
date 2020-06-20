3b3150becc3b9fe3fd974e65bd9f34d74323c648
Papryk <gustaw5022@gmail.com>
2020-06-20 19:21:17 +0200

    init project and base resources

---
+++ Flashcards/app/src/main/java/com/example/flashcards/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.flashcards
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flashcards.fragments.MainMenuFragment
+
+class MainActivity : AppCompatActivity(),
+    MainMenuFragment.OnFragmentInteractionListener {
+
+    private lateinit var mainMenuFragment: MainMenuFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainMenuFragment = MainMenuFragment.newInstance()
+
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(
+                R.anim.enter_bottom_to_top, R.anim.exit_top_to_bottom,
+                R.anim.enter_top_to_bottom, R.anim.exit_bottom_to_top
+            )
+            .add(R.id.main_frame_layout, mainMenuFragment)
+            .commit()
+
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+
+    }
+}
