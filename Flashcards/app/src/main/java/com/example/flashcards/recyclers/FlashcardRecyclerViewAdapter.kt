0acddc92d4cf899edfb7551f7b38cb5ce304a3a0
Papryk <gustaw5022@gmail.com>
2020-06-22 21:22:44 +0200

    delete works

--- Flashcards/app/src/main/java/com/example/flashcards/recyclers/FlashcardRecyclerViewAdapter.kt
+++ Flashcards/app/src/main/java/com/example/flashcards/recyclers/FlashcardRecyclerViewAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.flashcards.MainActivity
 import com.example.flashcards.R
+import com.example.flashcards.db.DataBaseHelper
 import com.example.flashcards.fragments.FlashcardsFragment
 import com.example.flashcards.models.Flashcard
 import kotlinx.android.synthetic.main.row_flashcard.view.*
@@ -25,6 +26,21 @@ class FlashcardRecyclerViewAdapter(var flashcardList: ArrayList<Flashcard>) :
     override fun onBindViewHolder(holder: FlascardViewHolder, position: Int) {
         holder.view.flashcard_cell_text_view_question.setText(flashcardList[position].question)
         holder.view.flashcard_cell_text_view_answer.setText(flashcardList[position].answer)
+        holder.view.flashcard_cell_image_view_delete.setOnClickListener {
+            holder.view.flashcard_cell_constraint_main.visibility = View.GONE
+            holder.view.flashcard_cell_constraint_delete.visibility = View.VISIBLE
+        }
+        holder.view.flashcard_cell_button_delete_no.setOnClickListener {
+            holder.view.flashcard_cell_constraint_main.visibility = View.VISIBLE
+            holder.view.flashcard_cell_constraint_delete.visibility = View.GONE
+        }
+        holder.view.flashcard_cell_button_delete_yes.setOnClickListener {
+            val dbHelper = DataBaseHelper(holder.view.context)
+            val result = dbHelper.deleteFlashcard(flashcardList[position].id)
+            flashcardList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, flashcardList.size)
+        }
     }
 
 }
